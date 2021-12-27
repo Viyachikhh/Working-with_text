7217d093d7b65993d2e9e80fed3cb748da165c63
Viyachikhh <vyachik2k1@gmail.com>
2021-12-27 10:08:08 +0300

    test commit

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import json
 
-#df = pd.read_csv('dataset_text/train.csv', header=[0, 1])
+df = pd.read_csv('dataset_text/train.csv', header=[0, 1])
 
 test1 = "\"Hi\""
 test2 = 'Hi, Hello'
@@ -15,18 +15,18 @@ re_uppercase = r'[A-ZА-Я]{1}\w+'
 re_abbrev = r'[A-ZА-Я]{2,}'
 re_name = r'[A-ZА-Я]\.'
 
-print(re.findall(re_quotes, test1))
-print(re.findall(re_uppercase, test2))
-print(re.findall(re_abbrev, test3))
-print(re.findall(re_name, test4))
+#print(re.findall(re_quotes, test1))
+#print(re.findall(re_uppercase, test2))
+#print(re.findall(re_abbrev, test3))
+#print(re.findall(re_name, test4))
 
-"""
+
 dict_ = dict()
 
 for i in range(df.shape[0]):
     string = df.iloc[i, 1]
     json_elem = {'sentence': string,
-                 'quotes': re.findall(re_quotes, string),
+                 'quotes': re.findall(re_quotes, string)[::2],
                  'uppercase': re.findall(re_uppercase, string),
                  'abbrev': re.findall(re_abbrev, string),
                  'names': re.findall(re_name, string)}
@@ -35,8 +35,8 @@ for i in range(df.shape[0]):
 with open('dataset_text/dataset.json', 'w') as f:
     json.dump(dict_, f)
 
-"""
-with open('dataset_text/dataset.json', 'r') as f:
-    dataset = json.load(f)
 
-print(dataset)
+#with open('dataset_text/dataset.json', 'r') as f:
+#    dataset = json.load(f)
+
+#print(dataset)
