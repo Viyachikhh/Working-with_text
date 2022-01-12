e33396ed910539cb0f651592a5eb01666d5da014
Viyachikhh <vyachik2k1@gmail.com>
2022-01-12 12:38:47 +0300

    updated preprocessing text: previous version did not find abbreviations. Now it is work

--- preprocessing.py
+++ preprocessing.py
@@ -1,42 +1,69 @@
 import pandas as pd
 import re
 import json
+import logging
 
-df = pd.read_csv('dataset_text/train.csv', header=[0, 1])
+logging.basicConfig(filename='logger.log', level=logging.DEBUG, filemode='w')
 
 test1 = "\"Hi\""
 test2 = 'Hi, Hello'
 test3 = u'Hi Hello AB A ZCGD АЛЁ'
 test4 = 'Б.Ю. Александор А. Колмогоров'
-
+test5 = '\n.'
 
 re_quotes = r'\".*?\"'
 re_uppercase = r'[A-ZА-Я]{1}\w+'
 re_abbrev = r'[A-ZА-Я]{2,}'
 re_name = r'[A-ZА-Я]\.'
 
-#print(re.findall(re_quotes, test1))
-#print(re.findall(re_uppercase, test2))
-#print(re.findall(re_abbrev, test3))
-#print(re.findall(re_name, test4))
+delete = r"([MD]r?s?\.|\[.\]|'\'+|\.+)"
 
 
-dict_ = dict()
+def make_dataset():
+    json_dict = dict()
+    count = 0
+    for i in range(100, 150):
+        string = re.sub(r'(\?+|\!+)', '.', df.iloc[i, 1])
+        logging.warning(f'first step: {string}')
+        string = re.sub(r'(\.{2,}|[-]+|[;:]+)', ' ', string)  # + ' '  delete ... ; - ;
+        logging.warning(f'second step: {string}')
+        string = re.sub(r'[^a-zA-Z0-9 \"\n\.]', '', string)  # delete excess symbols
+        logging.warning(f'third step: {string}')
+        sentences = re.split(r'\. ', string)
 
-for i in range(df.shape[0]):
-    string = df.iloc[i, 1]
-    json_elem = {'sentence': string,
-                 'quotes': re.findall(re_quotes, string)[::2],
-                 'uppercase': re.findall(re_uppercase, string),
-                 'abbrev': re.findall(re_abbrev, string),
-                 'names': re.findall(re_name, string)}
-    dict_[i] = json_elem
+        for sentence in sentences:
+            print('before: ', sentence, len(sentence))
+            if '.' in sentence:
+                sentence = re.sub(r'\.', '', sentence)
+            print('after: ', sentence)
+            if len(sentence) != 0:
+                if len(sentence) < 4:
+                    json_dict[count - 1]['name'].append(sentence + '.')
+                else:
+                    json_dict[count] = {'sentence': sentence,
+                                        'quotes': re.findall(re_quotes, sentence),
+                                        'uppercase': re.findall(re_uppercase, sentence),
+                                        'abbrev': re.findall(re_abbrev, sentence),
+                                        'name': re.findall(re_name, sentence)}
+                    count += 1
+            else:
+                continue
 
-with open('dataset_text/dataset.json', 'w') as f:
-    json.dump(dict_, f)
+    with open('dataset_text/dataset.json', 'w') as f:
+        json.dump(json_dict, f)
 
 
-#with open('dataset_text/dataset.json', 'r') as f:
-#    dataset = json.load(f)
+"""
+    Steps:
+    1) ?,! -> .
+    2) ..., -, $, ; -> ' '
+    3) split example by . (but exclude cases Dr., Mr., etc)
+    
+    
+"""
 
-#print(dataset)
+full_ = []
+df = pd.read_csv('dataset_text/rt_reviews.csv', encoding='1251')
+# print(df.head(10))
+
+make_dataset()
