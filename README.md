0eb5cfb0d71732198c30d0f2eec7143cbda521a8
Viyachikhh <vyachik2k1@gmail.com>
2022-01-17 11:04:50 +0300

    added example of csv for checking

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Working_with_text
+# Watching
 
 In current repository there will be functions to preprocessing text dataset and simple regex to create labels for dataset. 
+
+Labels - quotes, word with uppercase, Abbrev., cuts.
+
+rt_reviews.csv - test dataset with sentences (not preprocessing!) if you want to check workability of script
+
+Script read csv dataset and create another json with full sentence and list of labels from this dataset.
--- preprocessing.py
+++ preprocessing.py
@@ -19,11 +19,19 @@ re_name = r'[A-ZА-Я]\.'
 delete = r"([MD]r?s?\.|\[.\]|'\'+|\.+)"
 
 
-def make_dataset():
+def make_dataset(cvs_dataset):
     json_dict = dict()
     count = 0
-    for i in range(100, 150):
-        string = re.sub(r'(\?+|\!+)', '.', df.iloc[i, 1])
+    for i in range(cvs_dataset.shape[0]):
+        """
+            Steps:
+            1) ?,! -> .
+            2) ..., -, $, ; -> ' '
+            3) split example by . (but exclude cases Dr., Mr., etc)
+
+
+        """
+        string = re.sub(r'(\?+|\!+)', '.', cvs_dataset.iloc[i, 1])
         logging.warning(f'first step: {string}')
         string = re.sub(r'(\.{2,}|[-]+|[;:]+)', ' ', string)  # + ' '  delete ... ; - ;
         logging.warning(f'second step: {string}')
@@ -32,10 +40,10 @@ def make_dataset():
         sentences = re.split(r'\. ', string)
 
         for sentence in sentences:
-            print('before: ', sentence, len(sentence))
+            # print('before: ', sentence, len(sentence))
             if '.' in sentence:
                 sentence = re.sub(r'\.', '', sentence)
-            print('after: ', sentence)
+            # print('after: ', sentence)
             if len(sentence) != 0:
                 if len(sentence) < 4:
                     json_dict[count - 1]['name'].append(sentence + '.')
@@ -44,7 +52,8 @@ def make_dataset():
                                         'quotes': re.findall(re_quotes, sentence),
                                         'uppercase': re.findall(re_uppercase, sentence),
                                         'abbrev': re.findall(re_abbrev, sentence),
-                                        'name': re.findall(re_name, sentence)}
+                                        'name': re.findall(re_name, sentence)}  # now it is useless,
+                                                                                # 'cause i don't know how correct split
                     count += 1
             else:
                 continue
@@ -53,17 +62,6 @@ def make_dataset():
         json.dump(json_dict, f)
 
 
-"""
-    Steps:
-    1) ?,! -> .
-    2) ..., -, $, ; -> ' '
-    3) split example by . (but exclude cases Dr., Mr., etc)
-    
-    
-"""
-
-full_ = []
 df = pd.read_csv('dataset_text/rt_reviews.csv', encoding='1251')
-# print(df.head(10))
 
-make_dataset()
+make_dataset(df)
